14592ed64b2000243f04825513a33d52d6f26c31
생각하는 마리오네트 <66814045+ljs7463@users.noreply.github.com>
2023-09-26 22:14:00 +0900

    start fast api

---
+++ fastapi/main.py
@@ -0,0 +1,67 @@
+from enum import Enum
+from typing import Union
+from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/users/me")
+# async def read_user_me():
+#     return {"user_id": "the current user"}
+
+# @app.get("/users/{user_id}")
+# async def read_user(user_id: str):
+#     return {"uesr_id": user_id}
+
+# ========================================================
+
+# class ModelName(str, Enum):
+#     alexnet = "alexnet"
+#     resnet = "resnet"
+#     lenet = "lenet"
+
+
+# app = FastAPI()
+
+
+# @app.get("/models/{model_name}")
+# async def get_model(model_name: ModelName):
+#     if model_name is ModelName.alexnet:
+#         return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+#     if model_name.value == "lenet":
+#         return {"model_name": model_name, "message": "LeCNN all the images"}
+
+#     return {"model_name": model_name, "message": "Have some residuals"}
+
+# ========================================================
+# app = FastAPI()
+
+# @app.get("/files/{file_path:path}")
+# async def read_file(file_path: str):
+#     return {"file_path": file_path}
+
+
+# ========================================================
+
+
+# app = FastAPI()
+
+# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+# @app.get("/items/")
+# async def read_item(skip: int = 0, limit: int = 1):
+#     return fake_items_db[skip : skip + limit]
+
+
+# ========================================================
+
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
