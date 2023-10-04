b631b6ecf8940c4c31fb6303bfe6cbd501b775e9
생각하는 마리오네트 <66814045+ljs7463@users.noreply.github.com>
2023-10-04 18:35:08 +0900

    fix

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # app = FastAPI()
 
@@ -57,11 +58,108 @@ from fastapi import FastAPI
 # ========================================================
 
 
+# app = FastAPI()
+
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: Union[str, None] = None):
+#     if q:
+#         return {"item_id": item_id, "q": q}
+#     return {"item_id": item_id}
+
+
+# ========================================================
+
+# app = FastAPI()
+
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+#     item = {"item_id": item_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update(
+#             {"description": "This is an amazing item that has a long description"}
+#         )
+#     return item
+
+
+# ========================================================
+
+
+# app = FastAPI()
+
+
+# @app.get("/users/{user_id}/items/{item_id}")
+# async def read_user_item(
+#     user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+# ):
+#     item = {"item_id": item_id, "owner_id": user_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update(
+#             {"description": "This is an amazing item that has a long description"}
+#         )
+#     return item
+
+
+# ========================================================
+
+# app = FastAPI()
+
+
+# @app.get("/items/{item_id}")
+# async def read_user_item(item_id: str, needy: str):
+#     item = {"item_id": item_id, "needy": needy}
+#     return item
+
+
+# ========================================================
+# app = FastAPI()
+
+
+# @app.get("/items/{item_id}")
+# async def read_user_item(
+#     item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+# ):
+#     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+#     return item
+
+
+# ========================================================
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Union[str, None] = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
