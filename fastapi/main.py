97d15a913602c5904984c69077d563478121f0d2
생각하는 마리오네트 <66814045+ljs7463@users.noreply.github.com>
2023-10-09 21:10:47 +0900

    request body 연습

--- fastapi/main.py
+++ fastapi/main.py
@@ -130,6 +130,11 @@ from pydantic import BaseModel
 
 # ========================================================
 
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 class Item(BaseModel):
     name: str
@@ -141,25 +146,9 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
-
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
-app = FastAPI()
-
-
-app = FastAPI()
-
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
